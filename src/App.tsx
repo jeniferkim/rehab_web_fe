cfc96618c1a2f01158c982fbe256d5ecc3504d9b
jeniferkim <jeniferkim1673@naver.com>
2025-12-02 22:50:22 +0900

    feat: 전체 페이지 기본 UI 구현 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { AppLayout } from './components/layouts/AppLayout'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import RoutinesPage from './pages/RoutinesPage'
+import RoutineCreatePage from './pages/RoutineCreatePage'
+import RoutineEditPage from './pages/RoutineEditPage'
+import SettingsPage from './pages/SettingsPage'
+import CalendarPage from './pages/CalendarPage'
+import ChatPage from './pages/ChatPage'
 
 function App() {
 
   return (
     <>
-    
+    <BrowserRouter>
+      <Routes>
+          {/* 인증 전 페이지 */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+
+          {/* AppLayout이 적용되는 내부 페이지들 */}
+          <Route path="/app" element={<AppLayout />}>
+            <Route path="home" element={<HomePage />} />
+            <Route path="routines" element={<RoutinesPage />} />
+            <Route path="routines/new" element={<RoutineCreatePage />} />
+            <Route path="routines/:id" element={<RoutineEditPage />} />
+            <Route path="calendar" element={<CalendarPage />} />
+            <Route path="chat" element={<ChatPage />} />
+            <Route path="settings" element={<SettingsPage />} />
+          </Route>
+
+          {/* 기본 루트 → 로그인 또는 홈으로 리다이렉트 */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          {/* 그 외 404 대응 */}
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
